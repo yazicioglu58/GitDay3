2fcbf4638353fdec79ce7a8403978bad3b7958ed
Mehmet Yazicioglu <mehmetytx@gmail.com>
2019-02-10 22:37:22 -0600

    commit 3 and logout test

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -7,7 +7,9 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -16,7 +18,7 @@ public class LoginTests {
 
     WebDriver driver;
 
-    @BeforeClass
+    @BeforeMethod
     public void setUp(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
@@ -33,8 +35,18 @@ public class LoginTests {
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test"+ Keys.ENTER);
         Assert.assertEquals(driver.getTitle(), "Web Orders");
     }
-
     @Test
+    public void LogOutTest(){
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test"+ Keys.ENTER);
+        driver.findElement(By.id("ct100_logout")).click();
+        Assert.assertEquals(driver.getTitle(), "Web Orders Login");
+
+    }
+
+
+    @AfterMethod
     public void cleanUp(){
         driver.close();
 
