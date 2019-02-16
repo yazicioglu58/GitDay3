2c990d9d9b7c27300869c5d8e5b1be3f1867ae9e
Mehmet Yazicioglu <mehmetytx@gmail.com>
2019-02-16 06:47:24 -0600

    add LoginPage and comment

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -5,6 +5,7 @@ import net.bytebuddy.asm.Advice;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -12,8 +13,10 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
-
+//login tests are maintained here
+// without Page object approach
 public class LoginTests {
 
     WebDriver driver;
@@ -44,6 +47,32 @@ public class LoginTests {
 
     }
 
+        @Test
+        public void NameVerification() {
+            driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+            driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+            driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
+
+            WebElement nameVerify = driver.findElement(By.xpath("//td[.='Susan McLaren']"));
+            if (nameVerify.getText().equals("Susan McLaren")) {
+                System.out.println("Order verified");
+            } else {
+                System.out.println("Order did not verify");
+            }
+            verifyOrder(driver, "Bob Feather");
+        }
+            public static void verifyOrder(WebDriver driver, String orderName){
+
+
+                List<WebElement> allNames= driver.findElements(By.xpath("//table[@id='ctl00_MainContent_orderGrid']/tbody/tr/td[2]"));
+                for(WebElement n:allNames){
+                    System.out.println(n.getText());
+                    if(n.getText().equals(orderName)){
+                        System.out.println(orderName+"'s order verified");
+                        break;
+                    }
+                }
+        }
     @AfterMethod
     public void cleanUp(){
     driver.close();
